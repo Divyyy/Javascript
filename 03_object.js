cb7d1f93515021a89893916d318d71520485df1f
Divyam Srivastava <93554318+Divyyy@users.noreply.github.com>
2024-10-06 18:49:35 +0530

    Add files via upload

---
+++ 03_object.js
@@ -0,0 +1,9 @@
+// object de structure
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"div"
+}
+    const {courseInstructor:Instructor}= course
+    console.log(Instructor);
+    // concepts of API
