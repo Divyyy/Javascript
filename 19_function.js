7acb0ebffef05728a2cf15a9fd3a0d47b61d9353
Divyam Srivastava <93554318+Divyyy@users.noreply.github.com>
2024-10-07 23:20:27 +0530

    Add files via upload

---
+++ 19_function.js
@@ -0,0 +1,32 @@
+function name() {
+    console.log("D");
+    console.log("I");
+    console.log("V");
+    console.log("y");
+    console.log("A");
+    console.log("M");
+    
+    
+}
+// name()
+// function addTwoNumbers(number1, number2){
+// console.log(number1+number2);
+// }
+
+
+function addTwoNumbers(number1, number2){
+    // let result= number1+number2
+    // return result
+    return number1 + number2
+    }
+const result = addTwoNumbers(3,5)
+console.log("Result:", result);
+
+
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("divyam"));
+
+
